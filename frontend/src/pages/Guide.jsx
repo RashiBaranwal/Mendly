47443d3582e725cd6eee3afe149c2da4fde267d0
Rashi <rashi1704baranwal@gmail.com>
2025-08-31 23:24:12 +0530

    feat: adding guide page

---
+++ frontend/src/pages/Guide.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// function Guide() {
+//   return (
+//     <div>
+//       <h1 className='text-zinc-950'>Hello Welcome to guide page!</h1>
+//     </div>
+//   )
+// }
+
+// export default Guide
