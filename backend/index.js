d1c0b2bbc2fde7816a6b16c474c68850293c87e5
Rashhhiii <rashi1704baranwal@gmail.com>
2025-09-01 01:27:28 +0530

    Update CORS origin to include admin URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://mendly-frontend.onrender.com" , "http://localhost:5174"],
+ origin:["https://mendly-frontend.onrender.com" , "https://mendly-admin.onrender.com"],
  credentials:true
 }))
 
